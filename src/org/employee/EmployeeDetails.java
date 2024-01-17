981099d108819ced1d509b42a8ff6d040a40501b
anandgitacc <anand.akmail@gmail.com>
2024-01-17 17:02:06 +0530

    functional1

---
+++ src/org/employee/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package org.employee;
+
+public class EmployeeDetails { // body or block of the class
+	
+	// private -> access specifier
+	// void    -> return type of the given method
+	// empName -> method name
+	// ()      -> arguments place / input or parameter place
+	// {}      -> body or block of the method
+	private void empName() {
+		// implementation details or business logic
+		// double quotes "" -> String: predefined class
+		System.out.println("Employee name Anand");
+	}
+	
+	private void empId() {
+		System.out.println("employee id: 1122");
+	}
+	
+	// needed methods
+	public static void main(String[] args) {
+		// object creation
+		EmployeeDetails e = new EmployeeDetails();
+		e.empName();
+		e.empId();
+	}
+	
+
+}
